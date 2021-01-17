7abc0d17ba81adea96366b6456156fe8ae445786
Long Do <longdo@dobtech.vn>
2021-01-17 21:05:04 +0700

    changed zoom size

--- lib/helpers/location_helpers.dart
+++ lib/helpers/location_helpers.dart
@@ -5,6 +5,6 @@ class LocationHelpers {
     double longitude,
     double latitude,
   }) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$_GOOGLE_API_KEY';
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$_GOOGLE_API_KEY';
   }
 }
